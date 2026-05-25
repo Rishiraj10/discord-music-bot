82d2a280ca422cc1b3fb336f963f6cd565928ac3
RishiRaj <114611347+Rishiraj10@users.noreply.github.com>
2026-05-25 10:15:56 +0530

    Update MusicQueue.js

--- MusicQueue.js
+++ MusicQueue.js
@@ -181,6 +181,11 @@ class MusicQueue {
         filter: 'audioonly',
         quality: 'highestaudio',
         highWaterMark: 1 << 25,
+        requestOptions: {
+          headers: {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+          }
+        }
       });
       const resource = createAudioResource(stream, { inlineVolume: true });
       resource.volume.setVolume(this.volume);
